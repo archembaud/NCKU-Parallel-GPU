c9687d36dd05b05cfb4684bbe46a8817814b7704
Matt Smith <archembaud@hotmail.com>
2025-02-09 17:50:54 +1000

    Replace the tabs in demo 7 with spaces

--- 7_OMP_Hello_World/main.c
+++ 7_OMP_Hello_World/main.c
@@ -2,10 +2,10 @@
 #include <omp.h>
 
 int main(int argc, char *argv[]) {
-	// Create threads
-	#pragma omp parallel 
-	{
-		printf("Hello!\n");
-	} // Destroy threads 	
-	return 0;
+// Create threads
+    #pragma omp parallel
+    {
+        printf("Hello!\n");
+    } // Destroy threads
+    return 0;
 }
